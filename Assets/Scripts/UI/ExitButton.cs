62c3ca398b30cff78ce46edacd2e4d5a5ee9b5b4
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2023-11-09 23:28:52 +0100

    ExitButton

---
+++ Assets/Scripts/UI/ExitButton.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace UI
+{
+    public class ExitButton : MonoBehaviour
+    {
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    
+    }
+}
